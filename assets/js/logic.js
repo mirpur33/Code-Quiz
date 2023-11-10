9ddefc810b0f81761ddb7d0bfc1607936889bda9
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2023-11-10 21:05:57 +0000

    logic.js time code inputed

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,10 +4,20 @@ let timeLeft = 150;
 let questionsDiv = document.getElementById("questions");
 let startButton = document.getElementById("start");
 let startDiv = document.getElementById("start-screen");
+let timeEl = document.getElementById("time");
 
 startButton.onclick = beginQuiz;
 
 function beginQuiz(){
     console.log("start button clicked, begin quiz fuction called");
-   startDiv.setAttribute("class", "hide") 
+   startDiv.setAttribute("class", "hide");
+   
+   timer = setInterval(oneSecondclockHandler, 1000);
+   timeEl.textContent = timeLeft;
+
+}
+
+function oneSecondclockHandler(){
+    timeLeft --;
+    timeEl.textContent = timeLeft;
 }
