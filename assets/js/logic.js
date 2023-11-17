b413283c9de5d0c3958bc31957efa5c7ec7c6a04
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2023-11-17 17:52:43 +0000

    feedback questions given

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,6 +8,7 @@ let startDiv = document.getElementById("start-screen");
 let timeEl = document.getElementById("time");
 let questionEl = document.getElementById("question-title");
 let choicesEl = document.getElementById("choices");
+let feedbackEl = document.getElementById("feedback");
 
 startButton.onclick = beginQuiz;
 
@@ -48,5 +49,33 @@ presentQuestion.ansChoices.forEach(function(option, i) {
 }
 
 function ansChoiceClick(){
-    
+  if(this.value !== questions[qIndex].answer){
+    timeLeft -= 10;
+
+    if(timeLeft <0){
+        timeLeft = 0
+    }
+    feedbackEl.textContent = "Wrong Answer!!"
+
+    timeEl.textContent = timeLeft;
+  }  else {
+    feedbackEl.textContent = "Riight Answer!!"
+  }
+
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function(){
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+
+  qIndex ++;
+
+  if(qIndex === questions.length){
+    endQuiz();
+  } else {
+    showNextQuestion();
+  }
+}
+
+function endQuiz(){
+    //tbd
 }
