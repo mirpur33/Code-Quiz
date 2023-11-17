5529ceb8556a3df85ad45124ca68e8b1ff2f68f7
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2023-11-17 17:22:47 +0000

    To create a dummy questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,6 @@
 let timer;
 let timeLeft = 150;
+let qIndex = 0;
 
 let questionsDiv = document.getElementById("questions");
 let startButton = document.getElementById("start");
@@ -9,15 +10,20 @@ let timeEl = document.getElementById("time");
 startButton.onclick = beginQuiz;
 
 function beginQuiz(){
-    console.log("start button clicked, begin quiz fuction called");
+   console.log("start button clicked, begin quiz fuction called");
    startDiv.setAttribute("class", "hide");
-   
+   questionsDiv.removeAttribute("class");
    timer = setInterval(oneSecondclockHandler, 1000);
    timeEl.textContent = timeLeft;
+   showNextQuestion();
 
 }
 
 function oneSecondclockHandler(){
     timeLeft --;
     timeEl.textContent = timeLeft;
+}
+
+function showNextQuestion(){
+
 }
---
+++ assets/js/questions.js
@@ -0,0 +1,37 @@
+let questions = [
+    {
+        q: "question 1",
+        ansChoices: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 2"
+    },
+
+    {
+        q: "question 2",
+        ansChoices: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 1"
+    },
+
+    {
+        q: "question 3",
+        ansChoices: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 3"
+    },
+
+    {
+        q: "question 4",
+        ansChoices: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 4"
+    },
+
+    {
+        q: "question 5",
+        ansChoices: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 2"
+    },
+
+    {
+        q: "question 6",
+        ansChoices: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 3"
+    }
+];
